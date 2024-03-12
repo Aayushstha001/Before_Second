690b01b7766fabc683905ad7c036fcce7ac649fd
Aayushstha001 <153213991+Aayushstha001@users.noreply.github.com>
2024-03-12 19:43:28 +0545

    Login/Register

--- Kirana/main/views.py
+++ Kirana/main/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def home(request):
+    return render(request, 'main/home.html', {})
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'main/register.html', {'form': form})
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'main/login.html', {'error': 'Invalid login credentials'})
+    else:
+        return render(request, 'main/login.html')
+    
+def logout_user(request):
+    logout(request)
+    return redirect('home')
