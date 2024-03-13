ef20bb58f5b65092204f48ca1e0bd71d66e5e0af
rupeshram013 <rupeshram00995@gmail.com>
2024-03-13 07:48:29 +0545

    initialcommit_01

--- Kirana/Kirana/settings.py
+++ Kirana/Kirana/settings.py
@@ -108,9 +108,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATICFILES_DIRS = ["/home/rupeshram013/Desktop/codeme/kirana/Before_Second/Kirana/main/static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
