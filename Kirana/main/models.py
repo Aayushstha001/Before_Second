a8fbff07137f061111f7a87636ee20107533580f
rupeshram013 <rupeshram00995@gmail.com>
2024-03-14 18:37:14 +0545

    folder_structure_redesign_01

--- Kirana/main/models.py
+++ Kirana/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class form(models.Model):
+
+    username = models.CharField(max_length=30)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length= 30)
+    repassword = models.CharField(max_length= 30)
